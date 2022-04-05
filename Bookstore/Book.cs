a12494f664ea4c5c290a42396da7c1970ea2f423
Wiktoria <234762@edu.p.lodz.pl>
2022-04-05 13:42:43 +0200

    Book :)

--- Bookstore/Book.cs
+++ Bookstore/Book.cs
@@ -1,8 +1,25 @@
-﻿using System;
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
-public class Class1
-{
-	public Class1()
-	{
-	}
+
+namespace DataLayer 
+{ 
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Page_number { get; set; }
+        public float Price { get; set; }
+
+       
+        public Book(string title, string author, int page_number, float price)
+        {
+            Title = title;
+            Author = author;
+            Page_number = page_number;
+            Price = price;
+        }
+    }
 }
