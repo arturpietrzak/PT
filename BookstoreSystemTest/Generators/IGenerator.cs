9adcbed5a1be15aa00670c17bbc3a6eba8250911
ArtJSON <artur.pietrzak1@outlook.com>
2022-04-12 13:03:21 +0200

    Release

--- BookstoreSystemTest/Generators/IGenerator.cs
+++ BookstoreSystemTest/Generators/IGenerator.cs
@@ -5,11 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using BookstoreSystem.Data.API;
 
-namespace BookstoreSystem.Data
+namespace BookstoreSystemTest.Generators
 {
     public interface IGenerator
     {
         DataLayerAbstractAPI Generate();
-
     }
 }
