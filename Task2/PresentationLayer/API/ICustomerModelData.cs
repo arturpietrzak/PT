a0c378a29790f031ae3030d7f56a712036adba20
Wiktoria <234762@edu.p.lodz.pl>
2022-05-19 21:04:16 +0200

    Presentation Layer added, API impemented

---
+++ Task2/PresentationLayer/API/ICustomerModelData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.API
+{
+    public interface ICustomerModelData
+    {
+        int Customer_Id { get; }
+        string Name { get; }
+        string Surname { get; }
+    }
+}
